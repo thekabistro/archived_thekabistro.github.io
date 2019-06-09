78cd7451c1f52480bb59a239be495266dcfba12f
thekabistro <kabir@kabirkuriyan.com>
2019-06-09 13:08:56 -0400

    Updates

---
+++ precache-manifest.28bc1b2829b914b69132354db4414168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3a9ccd87fba24fde7a10ca30b0e9c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27cefcafd3907292d7b9",
+    "url": "/static/css/main.902ff192.chunk.css"
+  },
+  {
+    "revision": "02cf55ef77fd0923919e",
+    "url": "/static/js/2.f02f5704.chunk.js"
+  },
+  {
+    "revision": "27cefcafd3907292d7b9",
+    "url": "/static/js/main.a947cf0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
